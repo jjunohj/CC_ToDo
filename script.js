d9bad293a2a7d8935fbbb9c179e2c4ad47cab537
jjunohj <jjunohj@gmail.com>
2023-03-24 00:00:10 +0900

    Feat: 날씨에 따른 백그라운드 추가, 로컬스토리지 사용한 최적화

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 
 const createTodo = (storageData) => {
   let todoContents = todoInput.value;
@@ -34,11 +35,11 @@ const createTodo = (storageData) => {
   newList.appendChild(newSpan);
   todoList.appendChild(newList);
   todoInput.value = "";
-  console.log(saveItemsFn());
+  saveItemsFn();
 };
 
 const keyCodeCheck = () => {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
@@ -62,12 +63,84 @@ const saveItemsFn = () => {
   }
 
   saveItems.length === 0
-    ? localStorage.removeItem("save-items")
+    ? localStorage.removeItem("saved-items")
     : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+const weatherDataActive = ({ location, weather }) => {
+  const weatherMainList = [
+    "Clear",
+    "Cloud",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Thunderstorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+
+  locationNameTag.textContent = location;
+  document.body.style.backgroundImage = `url(./images/${weather}.jpg)`;
+
+  // 이전에 통신했던 지역과 지금의 지역이 다르다면 혹은 지역은 같은데 날씨가 다를 경우
+  if (
+    !savedWeatherData ||
+    savedWeatherData.location !== location ||
+    savedWeatherData.weather !== weather
+  ) {
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+const weatherSearch = ({ latitude, longitude }) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dfdde8911b4fd61d3cbd48fc46902e13`
+  )
+    .then((res) => {
+      // JSON.parse() response body만 존재할 때 사용가능, 응답 header도 존재하면 사용 불가
+      return res.json();
+    })
+    .then((json) => {
+      const weatherData = {
+        location: json.name,
+        weather: json.weather[0].main,
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+// 구조 분해 할당 !!
+const accessToGeo = ({ coords }) => {
+  const { latitude, longitude } = coords;
+  // Key와 value의 이름이 똑같을 때, shorthand property라고 한다.
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+
+  weatherSearch(positionObj);
+};
+
+const askForLocation = () => {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
 };
 
 if (savedTodoList) {
   for (let i = 0; i < savedTodoList.length; i++) {
     createTodo(savedTodoList[i]);
   }
+}
+
+askForLocation();
+
+if (savedWeatherData) {
+  weatherDataActive(savedWeatherData);
 }
